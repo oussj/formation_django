e7e9dab174c54827841dfa82ad637398a5b292c2
oussamah <oussamah.jaber@gmail.com>
2024-02-27 09:55:54 +0100

    add subtitle to book

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=1000, default="")
