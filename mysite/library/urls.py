a7a195579ee3268bb2b27c54b4a617c75cab5c93
oussamah <oussamah.jaber@gmail.com>
2024-02-27 12:31:52 +0100

    add rest routes for book

--- mysite/library/urls.py
+++ mysite/library/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 urlpatterns = [
     path("hello", views.hello_world, name="hello"),
+    path("books", views.add_book, name="add_book"),
+    path("books/<int:id>", views.delete)
 ]
+
+
+# GET    /books             ->  retrieve all books data
+# GET    /books/123         ->  retrieve the book with id 123
+# POST   /books    (+body)  ->  create one book with the given data
+# DELETE /books/123         ->  delete the book with id 123
--- mysite/library/views.py
+++ mysite/library/views.py
@@ -1,5 +1,42 @@
+from http.client import HTTPException
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 
+from library.models import Book
+from library.serializers import BookSerializer
+
+# to avoid TypeErrors : must receive a Request and return a Response
 def hello_world(request):
     return HttpResponse("Hello World")
+
+@csrf_exempt
+def add_book(request):
+    # add book
+    if request.method == "POST":
+        data = JSONParser().parse(request)
+
+        book_serializer = BookSerializer(data=data)
+        if book_serializer.is_valid():
+            book_serializer.save()
+        else:
+            return HTTPException()
+        
+        return JsonResponse(book_serializer.data)
+    
+    # get all books
+    if request.method == "GET":
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+@csrf_exempt
+def delete(request, id):
+    if request.method == "DELETE":
+        book = Book(id=id)
+        book.delete()
+        
+        
+        
